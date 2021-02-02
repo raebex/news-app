11477de2aa80611d0b5ea96f693b15bed56b4e9e
rebecca <r.young1025@gmail.com>
2021-02-02 12:53:58 -0600

    secure api creds

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,8 +2,9 @@ class Api::ArticlesController < ApplicationController
 
   def index
     @articles = HTTP
-      .auth("Bearer #{request.authorization}")
-      .get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2021-01-01&sortBy=publishedAt").parse["articles"]
+      # .auth(Rails.application.credentials.news_api[:api_key])
+      .headers("X-Api-Key" => Rails.application.credentials.news_api[:api_key])
+      .get("http://newsapi.org/v2/everything?q=#{params[:search]}&sortBy=publishedAt").parse["articles"]
     render "index.json.jb"
   end
 
